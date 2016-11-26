f48410bec383ff78c2eb9e0e72c72263100a274a
David Korff <davidkorff@gmail.com>
2016-11-26 23:44:01 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,22 +1,22 @@
 def first_challenge
-  contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-      knows: nil
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  }
+ contacts = {
+   "Jon Snow" => {
+     name: "Jon",
+     email: "jon_snow@thewall.we",
+     favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+     knows: nil
+   },
+   "Freddy Mercury" => {
+     name: "Freddy",
+     email: "freddy@mercury.com",
+     favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+   }
+ }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+ #your code here
+ contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if do |flavor|
+   flavor == 'strawberry'
+ end
+ #remember to return your newly altered contacts hash!
+ contacts
 end
-
